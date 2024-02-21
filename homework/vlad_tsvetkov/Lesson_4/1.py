ed66c96e1bf3d7e652629a7c6fa4b31252b7e8d3
Vlad Tsvetkov <tsvet91@gmail.com>
2024-02-21 15:36:51 +0300

    PEP fixes

--- homework/vlad_tsvetkov/Lesson_4/1.py
+++ homework/vlad_tsvetkov/Lesson_4/1.py
@@ -4,7 +4,7 @@ my_dict.update({'list': [_ for _ in range(5)]})
 my_dict.update({
     'dict': {'name': 'Alice Cooper', 'Born': '4.02.1948', 'Origin': 'Arizona',
              'Genre': 'Rock/Metal', 'Band': 'Alice Cooper'
-             }})       
+             }})
 my_dict.update({'set': {'black', 'white', 'blue', 'yellow', 'purple'}})
 print(my_dict['tuple'][-1])
 my_dict['list'].append(5)
