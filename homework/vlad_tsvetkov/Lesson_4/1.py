128e007b471276cc57a92738d5036ffc10c1615e
Vlad Tsvetkov <tsvet91@gmail.com>
2024-02-21 15:35:18 +0300

    PEP fixes

--- homework/vlad_tsvetkov/Lesson_4/1.py
+++ homework/vlad_tsvetkov/Lesson_4/1.py
@@ -2,10 +2,9 @@ my_dict = {'tuple': '', 'list': '', 'dict': '', 'set': ''}
 my_dict.update({'tuple': ('Cat', 'Dog', 'Turtle', 'Parrot', 'Ferret')})
 my_dict.update({'list': [_ for _ in range(5)]})
 my_dict.update({
-    'dict': {'name':'Alice Cooper', 'Born': '4.02.1948', 'Origin': 'Arizona', 
+    'dict': {'name': 'Alice Cooper', 'Born': '4.02.1948', 'Origin': 'Arizona',
              'Genre': 'Rock/Metal', 'Band': 'Alice Cooper'
-             }
-             })        
+             }})       
 my_dict.update({'set': {'black', 'white', 'blue', 'yellow', 'purple'}})
 print(my_dict['tuple'][-1])
 my_dict['list'].append(5)
