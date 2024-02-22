cad2f6147b94ef305b9da0708b0db2391c4648d9
Vlad Tsvetkov <tsvet91@gmail.com>
2024-02-22 13:11:00 +0300

    Fixed according to the requirements

--- homework/vlad_tsvetkov/Lesson_4/1.py
+++ homework/vlad_tsvetkov/Lesson_4/1.py
@@ -1,14 +1,12 @@
-my_dict = {'tuple': '', 'list': '', 'dict': '', 'set': ''}
-my_dict.update({'tuple': ('Cat', 'Dog', 'Turtle', 'Parrot', 'Ferret')})
-my_dict.update({'list': [_ for _ in range(5)]})
-my_dict.update({
-    'dict': {'name': 'Alice Cooper', 'Born': '4.02.1948', 'Origin': 'Arizona',
-             'Genre': 'Rock/Metal', 'Band': 'Alice Cooper'
-             }})
-my_dict.update({'set': {'black', 'white', 'blue', 'yellow', 'purple'}})
+my_dict = {'tuple': ('Cat', 'Dog', 'Turtle', 'Parrot', 'Ferret'),
+           'list': [0, 1, 2, 3, 4],
+           'dict': {'name': 'Alice Cooper', 'Born': '4.02.1948', 'Origin': 'Arizona',
+                    'Genre': 'Rock/Metal', 'Band': 'Alice Cooper'
+                    }, 'set': {'black', 'white', 'blue', 'yellow', 'purple'}}
 print(my_dict['tuple'][-1])
 my_dict['list'].append(5)
-my_dict['dict'].update({'i am a tuple': False})
+my_dict['list'].remove(1)
+my_dict['dict'].update({('i am a tuple',): False})
 my_dict['dict'].pop('Band')
 my_dict['set'].add('crimson')
 my_dict['set'].remove('yellow')
