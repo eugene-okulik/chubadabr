4a626f74744b825b6f118df315b4841f628fa06e
Vlad Tsvetkov <tsvet91@gmail.com>
2024-02-25 18:54:45 +0300

    Lesson 7 - loops and funcs

---
+++ homework/vlad_tsvetkov/Lesson_7/1.py
@@ -0,0 +1,13 @@
+def num_guesser():
+    print('Угадай, какое число загадано')
+    num = 777
+    while True:
+        guess = int(input("Ввежите число "))
+        if guess == num:
+            print("Поздравляю! Вы угадали!")
+            break
+        else:
+            print("попробуйте снова")
+
+
+num_guesser()
