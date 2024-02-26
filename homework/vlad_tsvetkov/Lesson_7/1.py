2e75d3d0dfd4a8c46b3d65087bdeb6a3001054d3
Vlad Tsvetkov <tsvet91@gmail.com>
2024-02-26 12:44:53 +0300

    typo fix

--- homework/vlad_tsvetkov/Lesson_7/1.py
+++ homework/vlad_tsvetkov/Lesson_7/1.py
@@ -2,7 +2,7 @@ def num_guesser():
     print('Угадай, какое число загадано')
     num = 777
     while True:
-        guess = int(input("Ввежите число "))
+        guess = int(input("Введите число "))
         if guess == num:
             print("Поздравляю! Вы угадали!")
             break
