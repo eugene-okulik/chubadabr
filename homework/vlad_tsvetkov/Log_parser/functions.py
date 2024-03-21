443cb44cbde3a54cf75aedc80d3eb39076b71aac
Vlad Tsvetkov <tsvet91@gmail.com>
2024-03-22 00:37:26 +0300

    Log Parser v2

--- homework/vlad_tsvetkov/Log_parser/functions.py
+++ homework/vlad_tsvetkov/Log_parser/functions.py
@@ -3,31 +3,28 @@ import collections
 from datetime import datetime
 from colorama import init, Fore, Style
 init()
+err_len = 300
+txt_wrap = 150
 
 
-def is_datetime(line):
+def get_datetime(line):
     try:
         date = line[:line.find(' ', line.find(' ') + 1)]
-        datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f')
-        return True
+        date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f')
+        return date
     except ValueError:
         return False
 
 
-def get_datetime(line):
-    date = datetime.strptime(line[:line.find(' ', line.find(' ') + 1)], '%Y-%m-%d %H:%M:%S.%f')
-    return date
-
-
 def get_log(line):
-    if is_datetime(line):
+    if get_datetime(line):
         return line[line.find(' ', line.find(' ') + 1) + 1:]
 
 
 def make_me_dict(file, logs):
     with open(file, newline='') as curr_file:
         for line in curr_file:
-            if is_datetime(line):
+            if get_datetime(line):
                 key = get_datetime(line)
                 logs[key] = get_log(line)
             else:
@@ -35,49 +32,62 @@ def make_me_dict(file, logs):
     return logs
 
 
-def date_search(logs, date):
-    result_logs = collections.defaultdict()
+def date_search(logs, date, result_logs=collections.defaultdict()):
+    # Cтрогое сравнение по дате
     if date.find('/') == -1 and date.find('..') == -1:
         req_datetime = datetime.strptime(date, '%Y-%m-%d %H:%M:%S.%f')
         if req_datetime in logs.keys():
-            print(Fore.YELLOW, req_datetime, Style.RESET_ALL, logs[req_datetime][:300])
-            print(Fore.GREEN)
-            answer = input("Wanna get full message? y/n ")
-            print(Style.RESET_ALL)
-            if answer == 'y':
-                print(Fore.YELLOW, req_datetime, Style.RESET_ALL, logs[req_datetime])
+            result_logs = {req_datetime: logs[req_datetime]}
+            return result_logs
         else:
             print(f'No logs for {req_datetime}')
+    # Позже указанной даты
     elif date.find('/') < date.find('..'):
         req_datetime = datetime.strptime(date[:-3], '%Y-%m-%d %H:%M:%S.%f')
-        result = list(filter(lambda key: key > req_datetime, logs.keys()))
-        result_logs = {date: logs[date] for date in result}
-        for key, value in result_logs.items():
-            print(Fore.YELLOW, key, Style.RESET_ALL, value[:300] if len(value) > 300 else value)
+        result_logs = dict(filter(lambda item: item[0] > req_datetime, logs.items()))
+        # result_logs = {date: logs[date] for date in result}
+        return result_logs
+    # Раньше указанной даты
     elif date.find('/') > date.find('..') and date.find('..') != -1:
         req_datetime = datetime.strptime(date[3:], '%Y-%m-%d %H:%M:%S.%f')
-        result = list(filter(lambda key: key < req_datetime, logs.keys()))
-        result_logs = {date: logs[date] for date in result}
-        for key, value in result_logs.items():
-            print(Fore.YELLOW, key, Style.RESET_ALL, value[:300] if len(value) > 300 else value)
+        result_logs = dict(filter(lambda item: item[0] < req_datetime, logs.items()))
+        # result_logs = {date: logs[date] for date in result}
+        return result_logs
+    # Между двумя датами
     elif date.find('/') > date.find('..') and date.find('..') == -1:
         start_date = datetime.strptime(date[:date.find('/')], '%Y-%m-%d %H:%M:%S.%f')
         end_date = datetime.strptime(date[date.find('/') + 1:], '%Y-%m-%d %H:%M:%S.%f')
-        result = list(filter(lambda key: (key > start_date and key < end_date), logs.keys()))
-        result_logs = {date: logs[date] for date in result}
+        result_logs = dict(filter(lambda item: (item[0] > start_date and item[0] < end_date), logs.items()))
+        # result_logs = {date: logs[date] for date in result}
+        return result_logs
+
+
+def text_search(logs, text='', notext=''):
+    # Есть только text
+    if bool(text) and not bool(notext):
+        result_logs = dict(filter(lambda item: text in item[1], logs.items()))
+    # Есть text и notext
+    if bool(text) and bool(notext):
+        result_logs = dict(filter(lambda item: (text in item[1] and notext not in item[1]), logs.items()))
+    # Есть только notext
+    if not bool(text) and bool(notext):
+        result_logs = dict(filter(lambda item: notext not in item[1], logs.items()))
+    return result_logs
+
+
+def printer(result_logs, full=False, text=''):
+    # При поиске по тексту - берём кусок с обеих сторон от текста
+    if bool(text):
         for key, value in result_logs.items():
-            print(Fore.YELLOW, key, Style.RESET_ALL, value[:300] if len(value) > 300 else value)
-
-
-def text_search(logs, text, notext=False):
-    # result_logs = collections.defaultdict()
-    for key, value in logs.items():
-        if value.find(text) != -1 and notext is False:
             text_ind = value.find(text)
-            start_text = value[:text_ind] if len(value[:text_ind]) < 150 else value[text_ind - 150:text_ind]
-            end_text = value[text_ind:] if len(value[text_ind:]) < 150 else value[text_ind:text_ind + 150]
-            print(Fore.YELLOW, key, Style.RESET_ALL, start_text + end_text)
-        elif notext is True:
-            for key, value in logs.items():
-                if text not in value:
-                    print(Fore.YELLOW, key, Style.RESET_ALL, value[:300])
+            start_text = value[:text_ind] if len(value[:text_ind]) < 150 else value[text_ind-150:text_ind]
+            end_text = value[text_ind:] if len(value[text_ind:]) < 150 else value[text_ind:text_ind+150]
+            print(Fore.YELLOW, key, Style.RESET_ALL, start_text+end_text)
+    # Был запрошен полный текст ошибки
+    elif full:
+        for key, value in result_logs.items():
+            print(Fore.YELLOW, key, Style.RESET_ALL, value)
+    # По умолчанию берём срез до 300 символов, наверное основной кейс должен быть верхним :)
+    else:
+        for key, value in result_logs.items():
+            print(Fore.YELLOW, key, Style.RESET_ALL, value[:err_len] if len(value) > err_len else value)
--- homework/vlad_tsvetkov/Log_parser/main.py
+++ homework/vlad_tsvetkov/Log_parser/main.py
@@ -3,20 +3,21 @@ import argparse
 import pathlib
 import collections
 
-from functions import make_me_dict, date_search, text_search
+from functions import make_me_dict, date_search, text_search, printer
 
 parser = argparse.ArgumentParser()
 parser.add_argument("path", nargs='+', type=pathlib.Path, help="File name")
 parser.add_argument("--date", help='''Search date "2022-02-03 00:51:29.616/.." / "../2022-02-03 00:51:29.616"
                     "2022-02-03 00:51:29.616/2022-02-03 01:51:29.616" / "2022-02-03 00:51:29.616" ''')
 parser.add_argument("--text", help="Text to search")
-parser.add_argument("--notext", help="Should not contain text", action="store_true")
+parser.add_argument("--full", help="Get full message for strict date", action="store_true")
+parser.add_argument("--notext", help="Should not contain this text")
 args = parser.parse_args()
 path = args.path[-1]
 date = args.date
 text = args.text
 notext = args.notext
-
+full = args.full
 logs = collections.defaultdict()
 # собрали словарь
 if os.path.isdir(path):
@@ -27,6 +28,8 @@ else:
     logs = make_me_dict(path, logs)
 # запустили поиск
 if date:
-    date_search(logs, date)
-elif text:
-    text_search(logs, text, notext)
+    logs = date_search(logs, date)
+if text or notext:
+    logs = text_search(logs, text, notext)
+
+printer(logs, full, text)
